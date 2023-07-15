8e7b15d1a4c192836aaeeba30f4f11a901a2916d
Naveenkumar123-hash <120244421+Naveenkumar123-hash@users.noreply.github.com>
2023-07-15 21:43:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -19,9 +19,8 @@ function Employee(name, age, jobTitle) {
 Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee;
 
-// Prototype method for Employee
-Employee.prototype.jobGreet = function () {
-  console.log(`Hello, my name is ${this.name} and I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+Person.prototype.greet = function () {
+  console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
 };
 
 // Usage example
