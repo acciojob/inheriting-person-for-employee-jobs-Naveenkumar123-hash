258383a0d8acc8cde787b588ea09d45046ab1485
Naveenkumar123-hash <120244421+Naveenkumar123-hash@users.noreply.github.com>
2023-07-15 21:33:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
 // complete this js code
 function Person(name, age) {
-	this.name=name;
-	this.age=age;
-	Person.Prototype.greet=function(){
-		console.log(Hello, my name is this.name, I am this.age years old);
-	}
+  this.name = name;
+  this.age = age;
 }
 
+// Prototype method for Person
+Person.prototype.greet = function () {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+};
+
+// Employee constructor function
 function Employee(name, age, jobTitle) {
-	this.name=name;
-	this.age=age;
-	this.jobTitle=jobTitle;
-	Employee.__proto__=Person.Prototype;
-	Employee.Prototype.jobGreet=function(){
-  console.log(Hello, my name is this.name, I am this.age years old, and my job title is this.jobTitle);
+  Person.call(this, name, age);
+  this.jobTitle = jobTitle;
 }
-}
+
+// Inherit Person prototype in Employee prototype
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
+
+// Prototype method for Employee
+Employee.prototype.jobGreet = function () {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+};
+
+// Usage example
+const person = new Person("John Doe", 25);
+person.greet(); // Output: Hello, my name is John Doe, I am 25 years old.
+
+const employee = new Employee("Alice", 30, "Software Engineer");
+employee.greet(); // Output: Hello, my name is Alice, I am 30 years old.
+employee.jobGreet();
 
 // Do not change code below this line
 window.Person = Person;
